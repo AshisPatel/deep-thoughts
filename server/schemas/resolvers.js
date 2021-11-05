e88acae6f7a2ed6de60c252614272467793b47dd
AP <ashis.n.patel@gmail.com>
2021-11-04 19:03:52 -0500

    Add potential for username sorting and reactions to thoughts query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,11 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
-        thoughts: async() => {
-            return Thought.find().sort({ createdAt: -1 })
+        // the first arguement is a placeholder, the second arguement is for parameters
+        thoughts: async(parent, { username }) => {
+            // if the username is passed in through the query, then we set our required params to the username, else we find all thoughts
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 })
         }
     }
 };
